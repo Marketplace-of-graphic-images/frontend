24b065977d7717c0df325a5a4865320058b1ca2c
Lex <Lexor.V.A.S@yandex.ru>
2023-08-16 19:02:22 +0300

    error-test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Main from '../pages/main/main';
 import './App.scss';
 
-const App = () => (
+const hpp = () => (
   <div className='App'>
     <header className='App-header'>
       <p>
