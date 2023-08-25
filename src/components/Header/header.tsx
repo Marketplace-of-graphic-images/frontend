38c0ec4e8e94d0354619dd083dc1fb203813c892
LepikOmsk <Lepik_a@mail.ru>
2023-08-25 17:01:09 +0300

    add buttonWithDropDown

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable react/no-array-index-key */
 import React from 'react';
 import style from './header.module.scss';
 import { useDispatch, useSelector } from '../../services/hooks';
 import { openModalAuth, openModalRegister } from '../../store';
 import { UniversalButton, LinkWordButton } from '../../ui-lib/Button';
+import ButtonWithDropDown from '../../ui-lib/Button/ButtonWithDropDown/ButtonWithDropDown';
+import { designLinks, photoLinks, videoLinks } from '../../constants/headerLinks';
 
 const Header = () => { 
   const dispatch = useDispatch();
@@ -17,6 +20,9 @@ const Header = () => {
     <header className={style.main}>
       <UniversalButton onClick={openModal1} type='button' size='small'> Создать аккаунт</UniversalButton>
       <LinkWordButton buttonName='Войти' onClick={openModal2} />
+
+      <ButtonWithDropDown title='Дизайн' menuItem={designLinks} />
+
       {/* <PasswordInput apiError='test' /> */}
       {/* <RegistrationPage /> */}
     </header>
