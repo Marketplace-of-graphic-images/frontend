b343f91d025906ed33b09b6f2d4dfc62f1b444a0
Veronika Sergienko <nichka030791@yandex.ru>
2023-08-30 19:03:16 +0600

    fix/fix import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,6 +13,7 @@ import {
 } from '../components/Forms';
 import PrivateRoute from '../services/PrivateRoute';
 import MainAuthor from '../pages/MainAuthor/MainAuthor';
+import Footer from 'components/Footer/Footer';
 
 const App = () => {
   const dispatch = useDispatch();
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,98 +1,98 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import style from './Footer.module.scss';
+import styles from './Footer.module.scss';
 import { UniversalButton } from '../../ui-lib/Button';
 
 const Footer = () => (
-  <footer className={style.footer}>
-    <div className={style.footer__container}>
-      <div className={style.footer__container_content}>
-        <div className={style.footer__menu}>
+  <footer className={styles.footer}>
+    <div className={styles.footer__container}>
+      <div className={styles.footer__container_content}>
+        <div className={styles.footer__menu}>
           <div>
-            <h4 className={style.footer__title}>«Маркетплейс»</h4>
-            <ul className={style.footer__links}>
-              <li className={style.footer__container_link}>
-                <div className={style.footer__icon} />
-                <Link className={style.footer__link} href='http://localhost:3000/'>О нас</Link>
+            <h4 className={styles.footer__title}>«Маркетплейс»</h4>
+            <ul className={styles.footer__links}>
+              <li className={styles.footer__container_link}>
+                <div className={styles.footer__icon} />
+                <Link className={styles.footer__link} href='http://localhost:3000/'>О нас</Link>
               </li>
-              <li className={style.footer__container_link}>
-                <div className={style.footer__icon} />
-                <Link className={style.footer__link} href='http://localhost:3000/'>FAQ</Link>
+              <li className={styles.footer__container_link}>
+                <div className={styles.footer__icon} />
+                <Link className={styles.footer__link} href='http://localhost:3000/'>FAQ</Link>
               </li>
-              <li className={style.footer__container_link}>
-                <div className={style.footer__icon} />
-                <Link className={style.footer__link} href='http://localhost:3000/'>Реквизиты</Link>
+              <li className={styles.footer__container_link}>
+                <div className={styles.footer__icon} />
+                <Link className={styles.footer__link} href='http://localhost:3000/'>Реквизиты</Link>
               </li>
-              <li className={style.footer__container_link}>
-                <div className={style.footer__icon} />
-                <Link className={style.footer__link} href='http://localhost:3000/'>Вакансии</Link>
+              <li className={styles.footer__container_link}>
+                <div className={styles.footer__icon} />
+                <Link className={styles.footer__link} href='http://localhost:3000/'>Вакансии</Link>
               </li>
-              <li className={style.footer__container_link}>
-                <div className={style.footer__icon} />
-                <Link className={style.footer__link} href='http://localhost:3000/'>Связаться с нами</Link>
+              <li className={styles.footer__container_link}>
+                <div className={styles.footer__icon} />
+                <Link className={styles.footer__link} href='http://localhost:3000/'>Связаться с нами</Link>
               </li>
             </ul>
           </div>
           <div>
-            <h4 className={style.footer__title}>Условия пользования</h4>
-            <ul className={style.footer__links}>
-              <li className={style.footer__container_link}>
-                <div className={style.footer__icon} />
-                <Link className={style.footer__link} href='http://localhost:3000/'>Об авторском праве</Link>
+            <h4 className={styles.footer__title}>Условия пользования</h4>
+            <ul className={styles.footer__links}>
+              <li className={styles.footer__container_link}>
+                <div className={styles.footer__icon} />
+                <Link className={styles.footer__link} href='http://localhost:3000/'>Об авторском праве</Link>
               </li>
-              <li className={style.footer__container_link}>
-                <div className={style.footer__icon} />
-                <Link className={style.footer__link} href='http://localhost:3000/'>Приложения</Link>
+              <li className={styles.footer__container_link}>
+                <div className={styles.footer__icon} />
+                <Link className={styles.footer__link} href='http://localhost:3000/'>Приложения</Link>
               </li>
-              <li className={style.footer__container_link}>
-                <div className={style.footer__icon} />
-                <Link className={style.footer__link} href='http://localhost:3000/'>Конфиденциальность</Link>
+              <li className={styles.footer__container_link}>
+                <div className={styles.footer__icon} />
+                <Link className={styles.footer__link} href='http://localhost:3000/'>Конфиденциальность</Link>
               </li>
-              <li className={style.footer__container_link}>
-                <div className={style.footer__icon} />
-                <Link className={style.footer__link} href='http://localhost:3000/'>Политика файлов Cookie</Link>
+              <li className={styles.footer__container_link}>
+                <div className={styles.footer__icon} />
+                <Link className={styles.footer__link} href='http://localhost:3000/'>Политика файлов Cookie</Link>
               </li>
-              <li className={style.footer__container_link}>
-                <div className={style.footer__icon} />
-                <Link className={style.footer__link} href='http://localhost:3000/'>Партнерская программа</Link>
+              <li className={styles.footer__container_link}>
+                <div className={styles.footer__icon} />
+                <Link className={styles.footer__link} href='http://localhost:3000/'>Партнерская программа</Link>
               </li>
             </ul>
           </div>
           <div>
-            <h4 className={style.footer__title}>Клиентам</h4>
-            <ul className={style.footer__links}>
-              <li className={style.footer__container_link}>
-                <div className={style.footer__icon} />
-                <Link className={style.footer__link} href='http://localhost:3000/'>Услуги</Link>
+            <h4 className={styles.footer__title}>Клиентам</h4>
+            <ul className={styles.footer__links}>
+              <li className={styles.footer__container_link}>
+                <div className={styles.footer__icon} />
+                <Link className={styles.footer__link} href='http://localhost:3000/'>Услуги</Link>
               </li>
-              <li className={style.footer__container_link}>
-                <div className={style.footer__icon} />
-                <Link className={style.footer__link} href='http://localhost:3000/'>Акции</Link>
+              <li className={styles.footer__container_link}>
+                <div className={styles.footer__icon} />
+                <Link className={styles.footer__link} href='http://localhost:3000/'>Акции</Link>
               </li>
-              <li className={style.footer__container_link}>
-                <div className={style.footer__icon} />
-                <Link className={style.footer__link} href='http://localhost:3000/'>Специальные предложения</Link>
+              <li className={styles.footer__container_link}>
+                <div className={styles.footer__icon} />
+                <Link className={styles.footer__link} href='http://localhost:3000/'>Специальные предложения</Link>
               </li>
-              <li className={style.footer__container_link}>
-                <div className={style.footer__icon} />
-                <Link className={style.footer__link} href='http://localhost:3000/'>Подписка</Link>
+              <li className={styles.footer__container_link}>
+                <div className={styles.footer__icon} />
+                <Link className={styles.footer__link} href='http://localhost:3000/'>Подписка</Link>
               </li>
             </ul>
           </div>
         </div>
-        <div className={style.footer__newsletter}>
-          <h4 className={style.footer__title}>
+        <div className={styles.footer__newsletter}>
+          <h4 className={styles.footer__title}>
             Подписывайтесь на рассылку, чтобы быть в курсе последних новостей и обновлений!
           </h4>
-          <input className={style.footer__input} aria-label='Email' type='email' placeholder='Email' />
+          <input className={styles.footer__input} aria-label='Email' type='email' placeholder='Email' />
           <UniversalButton size='small_medium'>Подписаться</UniversalButton>
         </div>
       </div>
-      <div className={style.footer__container_copyright}>
-        <p className={style.footer__copyright}>
+      <div className={styles.footer__container_copyright}>
+        <p className={styles.footer__copyright}>
           &copy; 2023 «Маркетплейс графических изображений»
         </p>
-        <p className={style.footer__copyright}>
+        <p className={styles.footer__copyright}>
           Все права защищены
         </p>
       </div>
