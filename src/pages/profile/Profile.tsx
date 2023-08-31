1ccbab5ec73119d327cf16f636429c294f633d0e
Lex <Lexor.V.A.S@yandex.ru>
2023-08-31 23:25:09 +0300

    add Profile

---
+++ src/pages/profile/Profile.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './profile.module.scss';
+
+const Profile = () => (
+  <main className={styles.main}>
+    <p> Профиль</p>
+
+  </main>
+);
+export default Profile;
