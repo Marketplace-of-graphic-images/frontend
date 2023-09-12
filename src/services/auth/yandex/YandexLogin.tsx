955e7d1a44b28ffa8b386ab2b244c79ec264c7a0
udartapkom <karakoom@ya.ru>
2023-09-12 07:42:34 +0300

    Yandex auth

--- src/services/auth/yandex/YandexLogin.tsx
+++ src/services/auth/yandex/YandexLogin.tsx
@@ -1,90 +1,62 @@
 /* eslint-disable camelcase */
-import React, { FC, ReactElement, useEffect } from 'react';
+import React, {
+  FC, ReactElement, useEffect, useState, 
+} from 'react';
 import HiddenFrame from './HiddenFrame';
 
 interface IYandexLogin {
   clientID: string
   children: ReactElement
 }
-
-const requestUrl = 'https://oauth.yandex.ru/authorize?response_type=token&client_id=';
+type TUrl = {
+  authorization_url: string
+};
+const redirectUri = 'https://pictura.acceleratorpracticum.ru/api/v1/accounts/profile/';
 
 const YandexLogin: FC<IYandexLogin> = ({ clientID, children }) => {
-  const getUserData = (token: string) => (fetch(`https://login.yandex.ru/info?oauth_token=${token}&format=json`, {
+  const [windowOpen, setWindowOpen] = useState('');
+  const getLink = () => (fetch(decodeURIComponent(`https://pictura.acceleratorpracticum.ru/api/v1/auth/social/o/yandex-oauth2/?${new URLSearchParams(
+    { redirect_uri: redirectUri },
+  ).toString()}`), {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
-  }));
- 
-  const handleMessageFromPopup = (event: MessageEvent) => {
-    const userKey = (event.data);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    const userObj = userKey.payload;
-    if (userObj) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      const userToken: string = userObj.access_token;
-      getUserData(userToken)
-        // eslint-disable-next-line promise/no-nesting
-        .then((res) => res.json()
-          .then((data) => console.log(data)))
-        .catch((err) => console.log(err));
-    }
-  }; 
- 
-  const getToken = () => {
-    const parts = window.location.href.split('#');
-    const queryPartUrl = parts.length > 1 && parts[1] !== 'frame' ? parts[1] : null;
-    if (!queryPartUrl) {
-      return null;
-    }
-    const result = {};
-    queryPartUrl.split('&').forEach((part) => {
-      const item = part.split('=');
-      result[item[0]] = decodeURIComponent(item[1]);
-    }); 
-    return result;
-  };
+  }))
+    .then((res: Response) => res.json())
+    .then((data: TUrl) => setWindowOpen(data.authorization_url))
+    .catch((err) => console.log(err));
+
+  const getCode = () => (
+    fetch(encodeURIComponent(windowOpen), {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  );
   
   const onLogin = () => {
     const h = 650;
     const w = 550;
     const y = window.outerHeight / 2 + window.screenY - (h / 2);
     const x = window.outerWidth / 2 + window.screenX - (w / 2);
-    window.open(`${requestUrl}${clientID}`, 'popup', `width=${w},height=${h},top=${y},left=${x}`);
-    window.addEventListener('message', handleMessageFromPopup, { once: true });
+      
+    // window.open(`${requestUrl}${clientID}`, 'popup',`width=${w},height=${h},top=${y},left=${x}`);
   };
-  let frameRedirectTo: null | string = null;
-
-  const accessKey = getToken();
-  const receiver: Window = window.parent !== window
-    ? window.parent
-    : window.opener;
-
   useEffect(() => {
-    if (accessKey && receiver) {
-      receiver.postMessage({
-        source: 'yandex-login',
-        payload: accessKey,
-      }, window.location.origin);
-      window.close();
-    }
-  });
-
-  if (!accessKey && !receiver) {
-    const autoLoginDIsabled = sessionStorage.getItem('yandexAutoLoginDisabled');
-    frameRedirectTo = autoLoginDIsabled !== 'true' 
-      ? requestUrl
-      : null;
-    window.addEventListener('message', handleMessageFromPopup, { once: false });
-  }
-
-  const cloned = React.cloneElement(children, { onClick: onLogin });
+    const h = 650;
+    const w = 550;
+    const y = window.outerHeight / 2 + window.screenY - (h / 2);
+    const x = window.outerWidth / 2 + window.screenX - (w / 2);
+    window.open(windowOpen, 'popup', `width=${w},height=${h},top=${y},left=${x}`);
+    console.log(windowOpen);
+  }, [windowOpen]);
+  const cloned = React.cloneElement(children, { onClick: getLink });
 
   return (
     <div>
       {cloned}
-      {frameRedirectTo && <HiddenFrame redirectTo={frameRedirectTo} />}
     </div>
   );
 };
