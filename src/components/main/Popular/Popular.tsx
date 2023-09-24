08547660e7f0d389c6f898f00040ea4a38cd8751
Alisbur <mail-4-eugen@yandex.ru>
2023-09-25 02:13:40 +0300

    feat: add Popular section rubber layout

--- src/components/main/Popular/Popular.tsx
+++ src/components/main/Popular/Popular.tsx
@@ -1,47 +1,45 @@
-import React, { FC } from 'react';
+import React from 'react';
 import style from './Popular.module.scss';
-import { Link } from 'react-router-dom';
-import testImg from '../../../assets/images/pictures/pic.jpg';
+import testImg from '../../../assets/images/pictures/404.png';
   
-const Popular: FC = () => (
+const Popular = () => (
   <section className={style.popular}>
     <div className={style.popular__content}>
-      <h2 className={style.popular__title}>Рекомендации:
+      <h2 className={style.popular__title}>
+        Рекомендации:
         <span className={style.popular__title_span}> специально для вас!</span>
       </h2>
 
       <div className={style.popular__imageItems}>
-        <div className={style.popular__mainlImageItem}>
-          <Link to="#">
-            <img src={testImg} className={style.popular__mainImage} alt="Котики" />
-            <span className={style.popular__mainImageCaption}>Котики</span>
-          </Link>
-        </div>
+        <button type='button' onClick={() => {}} className={style.popular__mainlImageButton}>
+          <img src={testImg} className={style.popular__mainImage} alt='Котики' />
+          <span className={style.popular__mainImageCaption}>Котики</span>
+        </button>
 
         <ul className={style.popular__fourImagesBlock}>
           <li className={style.popular__smallImageItem}>
-            <Link to="#">
-              <img src={testImg} className={style.popular__smallImage} alt="Офисные работники" />
+            <button type='button' onClick={() => {}} className={style.popular__smallImageButton}>
+              <img src={testImg} className={style.popular__smallImage} alt='Офисные работники' />
               <span className={style.popular__smallImageCaption}>Офисные работники</span>
-            </Link>
+            </button>
           </li>
           <li className={style.popular__smallImageItem}>
-            <Link to="#">
-              <img src={testImg} className={style.popular__smallImage} alt="Еда" />
+            <button type='button' onClick={() => {}} className={style.popular__smallImageButton}>
+              <img src={testImg} className={style.popular__smallImage} alt='Еда' />
               <span className={style.popular__smallImageCaption}>Еда</span>
-            </Link>
+            </button>
           </li>
           <li className={style.popular__smallImageItem}>
-            <Link to="#">
-              <img src={testImg} className={style.popular__smallImage} alt="Собачки" />
+            <button type='button' onClick={() => {}} className={style.popular__smallImageButton}>
+              <img src={testImg} className={style.popular__smallImage} alt='Собачки' />
               <span className={style.popular__smallImageCaption}>Собачки</span>
-            </Link>
+            </button>
           </li>
           <li className={style.popular__smallImageItem}>
-            <Link to="#">
-              <img src={testImg} className={style.popular__smallImage} alt="Мотивация" />
+            <button type='button' onClick={() => {}} className={style.popular__smallImageButton}>
+              <img src={testImg} className={style.popular__smallImage} alt='Мотивация' />
               <span className={style.popular__smallImageCaption}>Мотивация</span>
-            </Link>
+            </button>
           </li>
         </ul>
       </div>
