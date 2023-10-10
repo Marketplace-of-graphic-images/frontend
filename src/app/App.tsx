f36838d22e358f13051ca506f180522963c46fc8
Lex <Lexor.V.A.S@yandex.ru>
2023-10-11 01:09:01 +0300

    fix modal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Footer from 'components/Footer/Footer';
+import SharePopup from 'components/Template/SharePopup/SharePopup';
+import ComplainPopup from 'components/Template/ComplainPopup/ComplainPopup';
 import { 
   Main, Profile, YandexAuth, NotFound, ImageCardPage,
 } from '../pages';
@@ -8,6 +10,7 @@ import Popup from '../components/Template/Popup/Popup';
 import Header from '../components/Header/Header';
 import { useDispatch, useSelector } from '../services/hooks';
 import Loader from '../components/Loader/Loader';
+
 import {
   closeModal, isLoadingOff,
 } from '../store';
@@ -23,7 +26,7 @@ import checkAuth from '../thunks/chech-auth-thunk';
 const App = () => {
   const dispatch = useDispatch();
   const {
-    authModal, registerModal, passRecoveryModal, 
+    authModal, registerModal, passRecoveryModal, shareModal, complainModal, 
   } = useSelector((state) => state.modals);
   const { isLoading } = useSelector((state) => state.system);
 
@@ -75,6 +78,13 @@ const App = () => {
             <Footer />
           </>
         )}
+      <Popup isOpen={shareModal} onClose={closeModalState} width='625px' height='285px'>
+        <SharePopup />
+      </Popup>
+
+      <Popup isOpen={complainModal} onClose={closeModalState} width='555px'>
+        <ComplainPopup />
+      </Popup>
 
       <Popup isOpen={authModal} onClose={closeModalState}>
         <AuthorizationForm />
