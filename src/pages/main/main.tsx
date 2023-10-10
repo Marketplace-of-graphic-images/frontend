dbdd761bf752e31f4d2d413d98b0a2f0cda5a9af
Lex <Lexor.V.A.S@yandex.ru>
2023-10-11 00:59:46 +0300

    mini-fix

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import styles from './main.module.scss';
 import MainGuest from './components/MainGuest/MainGuest';
-import { UniversalButton } from '../../ui-lib/Button';
 import MainUser from './components/MainUser/MainUser';
 import MainAuthor from './components/MainAuthor/MainAuthor';
 import { useDispatch, useSelector } from '../../services/hooks';
-import { setAuthorRole, setUserRole } from '../../store';
 
 const Main = () => {
   const dispatch = useDispatch();
