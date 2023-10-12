5978233c4ebc84fb9ed4a6ff6e996c42f2fd9614
Darya Fedorova <dasha1234495@gmail.com>
2023-10-12 21:47:21 +0300

    feat: toggle like on favourites, possibility remove like from card

--- src/components/Profile/Content/components/ImageCardBig/ImageCardBig.tsx
+++ src/components/Profile/Content/components/ImageCardBig/ImageCardBig.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './ImageCardBig.module.scss';
 import { greyPlug } from '../../../../../assets/images/pictures';
 import { LikeButton } from '../../../../../ui-lib/Button';
+import { putLike, removeLike } from '../../../../../api/api';
 
 interface ImageCardBigProps {
   image: string;
@@ -11,15 +12,32 @@ interface ImageCardBigProps {
   isLiked: boolean;
 }
 
+/* eslint-disable @typescript-eslint/no-floating-promises, promise/catch-or-return */
+
 const ImageCardBig: FC<ImageCardBigProps> = ({ 
   image, name, id, isLiked, 
-}) => (
-  <li className={styles.imageCardBig}>
-    <Link to={`/card/${id}`}>
-      <LikeButton isLiked={isLiked} className={styles.imageCardBig__like} />
-      <img src={image || greyPlug} alt={name} className={styles.imageCardBig__img} />
-    </Link>
-  </li>
-);
+}) => {
+  const [like, setLike] = useState(isLiked);
+  
+  const toggleLike = () => {
+    if (like) {
+      removeLike(id).then(() => setLike(false));
+    } 
+    putLike(id).then(() => setLike(true));
+  };
+  
+  return (
+    <li className={styles.imageCardBig}>
+      <LikeButton
+        isLiked={like}
+        onClick={toggleLike}
+        className={styles.imageCardBig__like} />
+
+      <Link to={`/card/${id}`}>
+        <img src={image || greyPlug} alt={name} className={styles.imageCardBig__img} />
+      </Link>
+    </li>
+  );
+};
 
 export default ImageCardBig;
