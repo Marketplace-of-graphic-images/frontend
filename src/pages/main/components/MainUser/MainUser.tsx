93c264b2c34a7a9eb5b81353386e4852fe5cbfbf
Lex <Lexor.V.A.S@yandex.ru>
2023-10-13 15:28:09 +0300

    fix title popular and fix image and hidden profile

--- src/pages/main/components/MainUser/MainUser.tsx
+++ src/pages/main/components/MainUser/MainUser.tsx
@@ -39,7 +39,7 @@ const MainUser = () => {
             title='популярных категорий!' />
           <ImageCardsSection cards={popularPhotos} title='Популярные фотографии' link='/' isLoggedIn={isLoggedIn} />
           <ImageCardsSection cards={popularGifs} title='Популярные Gif' link='/' isLoggedIn={isLoggedIn} />
-          <ImageCardsSection cards={popularVectors} title='Популярные векторы' link='/' isLoggedIn={isLoggedIn} />
+          <ImageCardsSection cards={popularVectors} title='Популярные наборы иконок' link='/' isLoggedIn={isLoggedIn} />
         </>
       )}
 
